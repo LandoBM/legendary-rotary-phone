1311d98f27414c61371c342430ed44d79df1db02
Landon Murray <landonbmurray@icloud.com>
2023-05-07 00:22:52 -0400

    Finished header for disney app

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image } from 'react-native'
 import { NavigationBar} from 'react-native-navbar'
+// import { LinearGradient } from 'expo-linear-gradient'
 
 export default class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
-        <Text></Text>
+        {/* <Text style={styles.text}>Disney</Text> */}
+        <Image style={styles.logo} source = {require ('../images/disLogo.png')} />
       </View>
     )
   }
@@ -14,8 +16,26 @@ export default class Header extends Component {
 
 const styles = StyleSheet.create({
     header: {
-        backgroundColor:'#090b13',
+        backgroundColor: '#090b30',
         height: '10%',
         color:'white',
+    },
+
+    text: {
+        color:'white',
+        position: 'absolute',
+        top: '50%',
+        left: '40%',
+        fontSize: 25
+    },
+
+    logo: {
+        position: 'absolute',
+        width: 100,
+        height: 60,
+        resizeMode: 'stretch',
+        top: '20%',
+        left: '37%'
     }
+
 })
--- components/Home.js
+++ components/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <View>
-        <Text> Hello </Text>
+        <Text> Home Area </Text>
       </View>
     )
   }
